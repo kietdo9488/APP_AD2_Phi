ec536d336e560633b079d38ce746b104541bcd22
kietdo9488 <kietdo9488@gmail.com>
2024-03-27 20:03:15 +0700

    commit subnet

---
+++ app/src/main/java/com/example/myapplication/SanPhamByIdDanhMuc.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SanPhamByIdDanhMuc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_san_pham_by_id_danh_muc);
+    }
+}
